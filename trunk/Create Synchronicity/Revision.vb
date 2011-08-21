9977785a3ba9bee6d1f07a73d3195607fd79f632
createsoftware <createsoftware@0e38689e-7753-4358-a38e-9485ab89ba94>
2011-08-21 09:15:31 +0000

    Further refactoring/branding work.

--- trunk/Create Synchronicity/Revision.vb
+++ trunk/Create Synchronicity/Revision.vb
@@ -1,8 +1,3 @@
-﻿Module External
-    Public Const Build As Integer = 1
-    Public Const Website As String = "http://synchronicity.sourceforge.net/"
-    Public Const MainWeb As String = "http://createsoftware.users.sourceforge.net/"
-    Public Const Support As String = "mailto:createsoftware@users.sourceforge.net"
-    Public Const License As String = "http://www.gnu.org/licenses/gpl.html"
-    Public Const BugReport As String = "http://sourceforge.net/tracker/?group_id=264348&atid=1130882"
+﻿Friend Module Revision
+    Public Const Build As Integer = 1372
 End Module

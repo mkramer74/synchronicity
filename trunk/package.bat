7d2ce40b3c36f8b59b4a6281b8117a010116a33a
createsoftware <createsoftware@0e38689e-7753-4358-a38e-9485ab89ba94>
2012-10-28 10:43:12 +0000

    Upload release notes to project web since cross-service calls are now forbidden by sourceforge.

--- trunk/package.bat
+++ trunk/package.bat
@@ -45,16 +45,18 @@ echo (*) Uploading builds to frs.sourceforge.net and rev info to web.sourceforge
 		pscp "Create_Synchronicity-r%REV%.zip" "Create_Synchronicity-r%REV%-DEBUG.zip" "Create_Synchronicity_Setup-r%REV%.exe" "Create_Synchronicity-r%REV%-Linux.zip" "Create_Synchronicity-r%REV%-Extensions.zip" "Create_Synchronicity-r%REV%-Scripts.zip" "createsoftware,synchronicity@frs.sourceforge.net:/home/pfs/project/s/sy/synchronicity/Create Synchronicity/Unreleased (SVN Builds)" 2>> %LOG%
 	cd %ROOT%
 
-echo (*) Uploading SVN-CHECKSUMS to web.sourceforge.net
+echo (*) Uploading SVN-CHECKSUMS and Release notes to web.sourceforge.net
 (
 	echo.
 	echo -----
-	echo Uploading SVN-CHECKSUMS via SCP.
+	echo Uploading SVN-CHECKSUMS and Release notes via SCP.
 ) >> %LOG%
 	
 	cd %BUILD%
 		pscp SVN-CHECKSUMS "createsoftware,synchronicity@web.sourceforge.net:/home/groups/s/sy/synchronicity/htdocs/code" 2>> %LOG%
 	cd %ROOT%
+	
+	pscp "%ROOT%\Create Synchronicity\Release notes.txt" "createsoftware,synchronicity@web.sourceforge.net:/home/groups/s/sy/synchronicity/htdocs/code" 2>> %LOG%
 
 echo (*) Building manual and uploading it to web.sourceforge.net.
 (
